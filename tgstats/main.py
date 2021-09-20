ca06e214a62e0b710da7d9241ad13c45eef00034
ense-Nastydev <dev@ensenasty.club>
2021-09-20 03:42:04 -0700

    refactor

--- tgstats/main.py
+++ tgstats/main.py
@@ -1,110 +1,54 @@
 # -*- coding: utf-8 -*-
-"""TGroup_Stats.ipynb
-
-# Telegram chat analytics with Pandas
-
-This notebook is still a work in progress. we can now proces the machine
-readable json from a chat export file created with Telegram desktop. 
-"""
-
-
-for fn in uploaded.keys():
-    print(
-        'User uploaded file "{name}" with length {length} bytes'.format(
-            name=fn, length=len(uploaded[fn])
-        )
-    )
-
-"""# Data Processing
-
-make  lazy json loader and use it to feed a pandas dataframe.
-This dataframe will get transformed in a variety o ways for analysis
-
-### Helper Functions
-
-#### Module imports
-"""
 
 import itertools
 import json
-import math
 import numpy as np
+import os
 import pandas as pd
-import re
-import matplotlib.pyplot as plt
 
-plt.close("all")
-
-from boltons.iterutils import flatten
 from boltons.iterutils import windowed_iter
 from datetime import datetime
 from logzero import logger as log
-
-"""####
- Agregator functions
-"""
-
-regex = re.compile(r"type\s+text\s+")
+from tgstats.utils import text_stats, countnz, deltaaverage
+from tgstats.utils import fixurl
 
 
-def fixurl(txt):
-    ...
-    return txt
-
-
-def text_stats(txt):
-    if isinstance(txt, list):
-        txt = regex.sub(
-            "", " ".join(flatten(txt)), count=len(regex.findall(" ".join(flatten(txt))))
-        )
-    wordcount = len(txt.split(" "))
-    ...
-    return wordcount
-
-
-def countnz(arr):
-    return len([val for val in arr if not pd.isna(val)])
-
-
-def deltaaverage(arr):
-    sum = 0
-    narr = [val.seconds for val in arr if not pd.isna(val)]
-    if len(narr) > 0:
-        for e in narr:
-            sum += e
-        return sum / (len(narr) * 1.0)
-    else:
-        return 0
-
-
-"""#### A Deltas Generator
-
-"""
-
-
-def gendeltas(timestamps_object, objname):
+def gendeltas(timestamps_object, objname, seconds=False):
     """Generates a column with time deltas calculated from the current
     and previous timestamp value. First timedelta is always 0"""
     deltaidx = pd.RangeIndex(0, len(timestamps_object), name="align")
     a = datetime.now()
-    deltas = [a - a]
+    zerodelta = a - a
+    deltas = [zerodelta]
     for old, new in windowed_iter(timestamps_object, 2):
         # _, old,  _ = older
         # _, new,  _ = newer
         deltatime = datetime.fromisoformat(str(new)) - datetime.fromisoformat(str(old))
-        if deltatime >= (a - a):
+        if seconds:
+            deltatime = deltatime.total_seconds()
+            zerodelta = 0
+        if deltatime >= zerodelta:
             deltas.append(deltatime)
         else:
-            deltas.append(a - a)
+            deltas.append(zerodelta)
     deltasS = pd.Series(deltas, name=objname)
     return deltasS
     # return pd.DataFrame(deltasS, index=deltaidx)
 
 
-"""#### Text analysis helper funcions"""
+def data_generator(r):
+    all_messages = []
+    rowct = 0
+    with open(r, "r", encoding="utf8") as chat:
+        dataset = json.load(chat)
+        for conversation in dataset:
+            for msg in conversation["messages"]:
+                all_messages.append(fixtype(msg, conversation["title"], rowct))
+                rowct += 1
+    return (msg for msg in all_messages)
 
 
-def fixtype(msgdict):
+def fixtype(data, title, ordinal):
     """augments the message data object with calculated values from
     the messages own data. This is necessary to have a correctly typed index
     so we cast datetime.datetime from elegrams mercifully chosen ISO date string
@@ -112,30 +56,18 @@ def fixtype(msgdict):
     this data, you may need to customize the output to your chosen storage
     facility, Currently set up for Google Cloud Storage buckets.
     """
-
-    newmsg = msgdict.copy()
-    newmsg["date"] = datetime.fromisoformat(msgdict["date"])
-    if "photo" in msgdict.keys():
-        newmsg["photo"] = "fixurl({})".format(msgdict["photo"])
-    if "file" in msgdict.keys():
-        newmsg["file"] = "fixurl({})".format(msgdict["file"])
-    if "text" in msgdict.keys() and len(msgdict["text"]) > 0:
-        newmsg["wordcount"] = text_stats(msgdict["text"])
-    return newmsg
-
-
-def addidx_fixtype(data, ordinal):
-    """a shim to create an indexable column for the main dataframe, we want
-    to keep fixtype() as generic as possible"""
-    newdata = fixtype(data)
+    newdata = data.copy()
+    newdata["conv_grp"] = title
+    newdata["date"] = datetime.fromisoformat(data["date"])
+    if "photo" in data.keys():
+        newdata["photo"] = fixurl(data["photo"])
+    if "file" in data.keys():
+        newdata["file"] = fixurl(data["file"])
+    if "text" in data.keys() and len(data["text"]) > 0:
+        newdata["wordcount"] = text_stats(data["text"])
     newdata["align"] = ordinal
     return newdata
 
-
-"""### Main processing
-
-##### Important Constants
-"""
 
 aggregators = {
     "from": countnz,
@@ -143,7 +75,7 @@ aggregators = {
     "photo": countnz,
     "reply_to_message_id": countnz,
     # 'text':'string',
-    "wait_time": deltaaverage,
+    "wait_time": np.average,
     "wordcount": np.sum,
 }
 
@@ -154,77 +86,93 @@ typecasts = {
     "photo": "string",
     "reply_to_message_id": "Int64",
     "text": "string",
-    "wait_time": "timedelta64[ns]",
+    "wait_time": "seconds",
+    # "wait_time": "timedelta64[ns]", #use this to get pandas timedeltas
     "wordcount": "Int64",
 }
 
-"""#### This is where the saussage is made"""
 
+def create_dataframe(
+    src,
+    casts,
+    agregators,
+):
+    """Creates as dataframe containing chat info
 
-def data_generator():
-    all_messages = []
-    rowct = 0
-    for f in uploaded:
-        with open(f, "r", encoding="utf8") as chat:
-            conversation = json.load(chat)
-            for msg in conversation["messages"]:
-                all_messages.append(addidx_fixtype(msg, rowct))
-                rowct += 1
-    return (msg for msg in all_messages)
+    Parameters:
+        src: a data generator with Teleram chat data
+        casts: a dictionary of fields: types
+        aggregators: a dictionary of fields:aggregating function
 
-
-src = data_generator()
-for_dataframe, for_fields = itertools.tee(src)
-
-fielder = pd.DataFrame.from_records(
-    [m for m in for_fields],
-    index=["align"],
-)
-
-df = pd.DataFrame.from_records(
-    [m for m in for_dataframe],
-    index=["align"],
-    exclude=[excl for excl in list(fielder.columns) if excl not in typecasts.keys()],
-)
-
-df.fillna(np.nan, inplace=True)
-deltas = gendeltas(df.date, "wait_time")
-ddf = (
-    df.join(
-        deltas,
-        on="align",
+    Returns:
+        dataframe: conforming to fields declared in casts
+        with funcions for aggregation assigned according to
+        the aggregators array
+    """
+    log.debug("entering create_dataframe()")
+    # duplicate datastream from generator so you dont need to reset it
+    # when datastream is exhausted.
+    for_dataframe, for_fields = itertools.tee(src)
+    # fielder is a dataframe with all columns. its purpose is to generate
+    # column lists via inclusion or exclusion from a subset
+    fielder = pd.DataFrame.from_records(
+        [m for m in for_fields],
+        index=["align"],
+    )  # use the dataframe for field masking agains the list we know we need
+    excluded_fields = (
+        [excl for excl in list(fielder.columns) if excl not in typecasts.keys()],
     )
-    # .droplevel("align")
-    .astype(typecasts).sort_index()
-)
+    # build a new dataframe using the for_dataframe duplicated stream, add an
+    # add an index called allign so we can add new columns in their corresponding
+    # places.
+    df = pd.DataFrame.from_records(
+        [m for m in for_dataframe], index=["align"], exclude=excluded_fields
+    ).fillna(
+        np.nan
+    )  # empty fields get np.nan as a value
+    # now create a deltas vectpr by walking thrugh the column of datetimes
+    if casts["wait_time"] == "seconds":
+        seconds = True
+    deltas = gendeltas(df.date, "wait_time", seconds)  # when seconds ins false
+    # you get a column of pandas ditmedelta types, otherwise total seconds
+    # do the deed
+    ddf = (
+        df.join(  # joining
+            deltas,  # te deltas vector ... you get the idea
+            on="align",
+        )
+        .astype(typecasts)
+        .sort_index()
+    )
+    return ddf
 
-ddf.info()
 
-"""### Have a go analyzing the data on bigquery"""
+def bigquery(
+    datafile,
+    dataset=os.environ["BQDATASET"],
+    project=os.environ["GCPPROJECT"],
+    schema=[
+        {"name": "id", "type": "INTEGER"},
+        {"name": "text", "type": "STRING"},
+        {"name": "wordcount", "type": "INTEGER"},
+        {"name": "reply_to_message_id", "type": "INTEGER"},
+        {"name": "photo", "type": "STRING"},
+        {"name": "wait_time", "type": "FLOAT"},
+    ],
+):
 
-import os
+    log.info("creating bigquery dataset")
+    src = data_generator(datafile)
+    chatinfo = create_dataframe(src, typecasts, aggregators)
+    ts = chatinfo.to_gbq(
+        "{}".format(dataset),
+        project_id="{}".format(project),
+        if_exists="replace",
+        table_schema=schema,
+    )
 
-dataset = os.environ["BQDATASET"]
-project = os.environ["GCPPROJECT"]
-schema = [
-    {"name": "id", "type": "INTEGER", "mode": "NULLABLE"},
-    {"name": "text", "     type": "STRING", "mode": "NULLABLE"},
-    {"name": "wordcount", "type": "INTEGER", "mode": "NULLABLE"},
-    {"name": "reply_to_message_id", "type": "INTEGER", "mode": "NULLABLE"},
-    {"name": "photo", "    type": "STRING", "mode": "NULLABLE"},
-    {"name": "wait_time", "type": "STRING", "mode": "NULLABLE"},
-]
 
-ts = ddf.to_gbq(
-    "{}".format(dataset),
-    project_id="{}".format(project),
-    if_exists="replace",
-    table_schema=schema,
-)
-
-"""## Visualizations derived from the built dataframe"""
-
-ts = ddf
-ts.reindex(ts["date"])
-daily = ts.resample("D")
-daily.agg(sum)
+##  ts = ddf
+# ts.reindex(ts["date"])
+# daily = ts.resample("D")
+# daily.agg(sum)
